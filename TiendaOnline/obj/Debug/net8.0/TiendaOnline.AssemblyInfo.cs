6323faab8fcd4c0bb0b3354818f609b7e94435e1
PabloIgeiNakagawa <pabloigeinaka@gmail.com>
2025-07-08 16:22:33 -0300

    Modifique el home, una cosa del carrito y de la navbar

--- TiendaOnline/obj/Debug/net8.0/TiendaOnline.AssemblyInfo.cs
+++ TiendaOnline/obj/Debug/net8.0/TiendaOnline.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TiendaOnline")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce425750ccffa884339140424d5949891dcb2618")]
 [assembly: System.Reflection.AssemblyProductAttribute("TiendaOnline")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TiendaOnline")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
