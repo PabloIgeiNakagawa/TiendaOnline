65610e595fcb386adcd3b45e30ba2902948d7a11
PabloIgeiNakagawa <pabloigeinaka@gmail.com>
2026-01-28 19:21:49 -0300

    Create AuditoriaController.cs

---
+++ TiendaOnline/Areas/Admin/Controllers/AuditoriaController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TiendaOnline.Services.IServices;
+
+
+namespace TiendaOnline.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles= "Administrador")]
+    public class AuditoriaController : Controller
+    {
+        private readonly IAuditoriaService _auditoriaService;
+
+        public AuditoriaController(IAuditoriaService auditoriaService)
+        {
+            _auditoriaService = auditoriaService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var model = await _auditoriaService.ObtenerAuditoriasAsync();
+            return View(model);
+        }
+    }
+}
